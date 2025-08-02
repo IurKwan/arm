3d2a3136fe763bc6914bdb1e7c59caed06d947d2
zhirui.guan <guanzhirui@gmail.com>
2025-08-03 01:26:09 +0800

    refactor(anim): 实现FragmentAnimator的Parcelable接口以支持序列化

--- arm-fragment/core/src/main/java/com/iur/arm/fragment/core/anim/DefaultNoAnimator.kt
+++ arm-fragment/core/src/main/java/com/iur/arm/fragment/core/anim/DefaultNoAnimator.kt
@@ -1,10 +1,26 @@
 package com.iur.arm.fragment.core.anim
 
-class DefaultNoAnimator : FragmentAnimator() {
+import android.os.Parcel
+import android.os.Parcelable
+
+class DefaultNoAnimator() : FragmentAnimator() {
     init {
         enter = 0
         exit = 0
         popEnter = 0
         popExit = 0
+    }
+
+    constructor(parcel: Parcel) : this() {
+        enter = parcel.readInt()
+        exit = parcel.readInt()
+        popEnter = parcel.readInt()
+        popExit = parcel.readInt()
+    }
+
+    companion object CREATOR : Parcelable.Creator<DefaultNoAnimator> {
+        override fun createFromParcel(parcel: Parcel): DefaultNoAnimator = DefaultNoAnimator(parcel)
+
+        override fun newArray(size: Int): Array<DefaultNoAnimator?> = arrayOfNulls(size)
     }
 }
