098baff73f13907ed1b61fdf91714021b50184b7
IurKwan <guanzhirui@gmail.com>
2025-11-12 15:46:18 +0800

    build: 更新arm-fragment-core版本号至0.0.2

--- arm-fragment/core/build.gradle.kts
+++ arm-fragment/core/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "io.github.iur"
             artifactId = "arm-fragment-core"
-            version = "0.0.1"
+            version = "0.0.2"
 
             afterEvaluate {
                 from(components["release"])
