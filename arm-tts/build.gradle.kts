2b8f9b78c59957b1fca94694310cf6bf75266090
IurKwan <guanzhirui@gmail.com>
2025-12-25 09:49:44 +0800

    build: v1.0.3

--- arm-tts/build.gradle.kts
+++ arm-tts/build.gradle.kts
@@ -80,7 +80,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "io.github.iur"
             artifactId = "arm-tts"
-            version = "1.0.2"
+            version = "1.0.3"
 
             afterEvaluate {
                 from(components["release"])
