838d59f5bec19e288d31665589ca00bfcaecaf2f
IurKwan <guanzhirui@gmail.com>
2026-01-15 15:57:22 +0800

    build: 更新版本号至1.0.6

--- arm-keyboard/build.gradle.kts
+++ arm-keyboard/build.gradle.kts
@@ -78,7 +78,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "io.github.iur"
             artifactId = "arm-keyboard"
-            version = "1.0.5"
+            version = "1.0.6"
 
             afterEvaluate {
                 from(components["release"])
