cb1635abe0370311586d2718c836e9672a4ac4c3
IurKwan <guanzhirui@gmail.com>
2026-06-14 18:24:42 +0800

    refactor(arm-modbus): migrate ModbusRespException to Kotlin

---
+++ arm-modbus/src/main/java/io/github/iur/arm/modbus/ModbusRespException.kt
@@ -0,0 +1,11 @@
+package io.github.iur.arm.modbus
+
+import com.serotonin.modbus4j.msg.ModbusResponse
+
+/**
+ * Modbus 响应异常
+ */
+class ModbusRespException(response: ModbusResponse) :
+    Exception("${response.exceptionMessage} ,code=${response.exceptionCode}") {
+    val exceptionCode: Byte = response.exceptionCode
+}
