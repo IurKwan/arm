2fb1cb3252770abb8cb54797a3ef930e03029417
IurKwan <guanzhirui@gmail.com>
2026-06-23 12:20:38 +0800

    build: v1.0.2

--- arm-modbus/build.gradle.kts
+++ arm-modbus/build.gradle.kts
@@ -86,7 +86,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "io.github.iur"
             artifactId = "arm-modbus"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
