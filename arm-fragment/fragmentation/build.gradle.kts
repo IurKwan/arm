34fe65edaff0d66276552766c6d86edd3bbea38b
IurKwan <guanzhirui@gmail.com>
2026-08-20 10:51:11 +0800

    chore: unify arm fragment release version

--- arm-fragment/fragmentation/build.gradle.kts
+++ arm-fragment/fragmentation/build.gradle.kts
@@ -54,7 +54,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "io.github.iur"
             artifactId = "arm-fragment-fragmentation"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
