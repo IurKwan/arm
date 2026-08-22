7da7b6b3d971fe304051443594e4daf862308b50
IurKwan <guanzhirui@gmail.com>
2026-08-22 13:19:41 +0800

    chore(arm-modbus): bump version to 1.0.5

--- arm-modbus/build.gradle.kts
+++ arm-modbus/build.gradle.kts
@@ -86,7 +86,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "io.github.iur"
             artifactId = "arm-modbus"
-            version = "1.0.4"
+            version = "1.0.5"
 
             afterEvaluate {
                 from(components["release"])
